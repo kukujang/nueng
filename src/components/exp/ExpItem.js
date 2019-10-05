239ae9b8907b8336aaff7825c51946ccdb0ac5cc
Nueng <kukujang@hotmail.com>
2019-10-06 00:09:29 +0700

    review code json

--- src/components/exp/ExpItem.js
+++ src/components/exp/ExpItem.js
@@ -3,6 +3,11 @@ import React, { Component } from "react";
 class ExpItem extends Component {
 	render() {
 		const { company, photo, photo2x, when, desc } = this.props;
+
+		function createMarkup() {
+			return {__html: desc};
+		}
+
 		return (
 			<div className="exp-item">
 				<h2 className="head-blue font-style-4">
@@ -12,7 +17,7 @@ class ExpItem extends Component {
 				</h2>
 				{photo===""?"":<div className="pic"><img src={photo} srcSet={photo2x+' 2x'} alt={company} /></div>}
 				<h3 className="head-subpage font-style-4">รายละเอียด</h3>
-				{desc}
+				<div className="desc" dangerouslySetInnerHTML={createMarkup()} ></div>
 			</div>
 		);
 	}
